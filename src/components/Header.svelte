d6f48b54c7f88e841a8ff231cd5334cb69b7d06e
FarazSiddi <f1.siddiqui@yandex.com>
2025-01-15 20:59:08 -0500

    Added Header Bar

---
+++ src/components/Header.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let y;
+
+    let tabs = [
+        { name: "Stats", link: "#stats" },
+        { name: "Settings", link: "#settings" },
+    ];
+</script>
+
+<header class={"sticky z-[10] top-0 duration-200 px-6 py-6 flex items-center justify-between border border-solid bg-slate-300 border-slate-900 text-slate-900"}>
+    <h1 class="font-medium">
+        Welcome, <b class="font-bold poppins">Username</b>!
+    </h1>
+    <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
+        {#each tabs as tab, index}
+          <a class="duration-200 hover:text-cyan-500" href={tab.link}>
+            <p>{tab.name}</p>
+          </a>
+        {/each}
+    </div>
+    <a href="#logout" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-neutral-950">
+        <div class="absolute top-0 right-full w-full h-full bg-cyan-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+
+        </div>
+        <h4 class="relative z-9">Log Out</h4>
+    </a>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,17 +12,10 @@
     }
   </script>
   
-  <!-- <div class="relative flex flex-col max-w-[1920px] mx-auto w-full text-sm sm:text-base min-h-screen">
-      <div class={"fixed bottom-0 w-full duration-200 flex p-10 z-[10] " + (
-          y > 0 ? ' opacity-full pointer-events-auto ': ' pointer-events-none-opacity-0 '
-      )}>
-          <button on:click={goTop} class="ml-auto rounded-full aspect-square bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer" aria-label="Go to top">
-              <i class="fa-solid fa-arrow-up grid place-items-center aspect-square"></i>
-          </button>
-      </div>
+  <div class="relative flex flex-col max-w-[1920px] mx-auto w-full text-sm sm:text-base min-h-screen">
       <Header y={y}/>
       <slot />
-      <Footer />
-  </div> -->
+      <!-- <Footer /> -->
+  </div>
   
   <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
